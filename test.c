757c2216768895511b46acdb378d08aaa5e70019
wyy511511 <wuyaoyao0309@163.com>
2023-03-05 23:09:00 -0700

    test

--- test.c
+++ test.c
@@ -14,61 +14,32 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Ask the user whether they want to read or write to the device
-    char action;
-    printf("Do you want to (r)ead or (w)rite to the device? ");
-    getchar();
-    scanf("%c", &action);
+    // Read from the device
+    int num_bytes;
+    printf("Enter the number of bytes you want to read: ");
+    scanf("%d", &num_bytes);
 
-    if (action == 'r') {
-        // Read from the device
-        int num_bytes;
-        printf("Enter the number of bytes you want to read: ");
-        scanf("%d", &num_bytes);
+    char *buffer = malloc(num_bytes + 1); // +1 for null terminator
 
-        char *buffer = malloc(num_bytes + 1); // +1 for null terminator
-
-        if (buffer == NULL) {
-            printf("Error: failed to allocate buffer\n");
-            exit(EXIT_FAILURE);
-        }
-
-        ssize_t bytes_read = read(device_fd, buffer, num_bytes);
-
-        if (bytes_read == -1) {
-            printf("Error: failed to read from device file\n");
-            exit(EXIT_FAILURE);
-        }
-
-        buffer[bytes_read] = '\0'; // Null-terminate the buffer
-
-        printf("%s\n", buffer);
-
-        // Free the buffer
-        free(buffer);
-    } else if (action == 'w') {
-        // Write to the device
-        printf("Enter the string you want to write: ");
-
-        char input[100];
-        fgets(input, sizeof(input), stdin); // Read a line of input from the user
-
-        ssize_t bytes_written = write(device_fd, input, strlen(input));
-
-        if (bytes_written == -1) {
-            printf("Error: failed to write to device file\n");
-            exit(EXIT_FAILURE);
-        } else {
-            printf("Successfully wrote %zd bytes to device\n", bytes_written);
-        }
-    } else {
-        // Invalid action
-        printf("Error: invalid action\n");
+    if (buffer == NULL) {
+        printf("Error: failed to allocate buffer\n");
         exit(EXIT_FAILURE);
     }
+
+    ssize_t bytes_read = read(device_fd, buffer, num_bytes);
+
+    if (bytes_read == -1) {
+        printf("Error: failed to read from device file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[bytes_read] = '\0'; // Null-terminate the buffer
+
+    printf("%s\n", buffer);
 
     // Close the device file
     close(device_fd);
 
     return 0;
 }
+
---
+++ testall.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <device file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp = fopen(argv[1], "r+");
+
+    if (fp == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Choose an option (r for read, w for write, s for seek): ");
+        char option = getchar();
+        while (getchar() != '\n'); // consume remaining input
+
+        switch (option) {
+            case 'r':
+                printf("Enter the number of bytes you want to read: ");
+                size_t nbytes;
+                scanf("%zu", &nbytes);
+                while (getchar() != '\n'); // consume remaining input
+
+                char *buffer = malloc(nbytes);
+                if (buffer == NULL) {
+                    perror("malloc");
+                    exit(EXIT_FAILURE);
+                }
+
+                size_t nread = fread(buffer, 1, nbytes, fp);
+                if (nread < nbytes) {
+                    if (feof(fp)) {
+                        printf("End of file reached\n");
+                    } else if (ferror(fp)) {
+                        perror("fread");
+                    }
+                } else {
+                    printf("%.*s\n", (int) nread, buffer);
+                }
+
+                free(buffer);
+                break;
+
+            case 'w':
+                printf("Enter the string you want to write: ");
+                char buf[1024];
+                fgets(buf, sizeof(buf), stdin);
+                size_t len = strlen(buf);
+                if (buf[len-1] == '\n') {
+                    buf[len-1] = '\0';
+                    len--;
+                }
+
+                size_t nwritten = fwrite(buf, 1, len, fp);
+                if (nwritten < len) {
+                    perror("fwrite");
+                }
+                break;
+
+            case 's':
+                printf("Enter an offset value: ");
+                long offset;
+                scanf("%ld", &offset);
+                while (getchar() != '\n'); // consume remaining input
+
+                printf("Enter a value for whence (0 for SEEK_SET, 1 for SEEK_CUR, 2 for SEEK_END): ");
+                int whence;
+                scanf("%d", &whence);
+                while (getchar() != '\n'); // consume remaining input
+
+                int res = fseek(fp, offset, whence);
+                if (res != 0) {
+                    perror("fseek");
+                }
+                break;
+
+            default:
+                continue;
+        }
+
+        if (option == EOF) {
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
